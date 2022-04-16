285e1f1a7862e9d719405b2c2da522ab71c188d6
rickytrevor <rick@rickyscloud.com>
2022-04-16 23:06:30 +0200

    experimental

--- borg.sh
+++ borg.sh
@@ -1,60 +1,75 @@
-#!/bin/bash
-export BORG_PASSCOMMAND="cat /path/to/borgpass"
-SSHPASSFILE=/path/to/sshpasswordfile
-BORGSERVER=ssh://user@ip:port
-PATTERN=id_ed25519
-REMOTEPATH=/path/to/remote/borg
-BACKUPNAME=backupname
+function help(){
+    echo "usage:"
+    echo "  borg.sh [options] "
+    echo "options:"
+    echo "list"
+    echo "create + name"
+    echo "delete + name"
+    echo "reconfigure"
 
-arg2err () {
-   if [[ -z "$2" ]]
-   then
-       echo "Archive name not provided!"
-       exit 2
-   fi
+}
+function reconfigure(){
+    rm $FILE
+    setup 
+}
+function list(){
+    declare -a array
+    i=0
+    pwdlogin=0
+    while IFS= read -r line; do
+        #crop everything before = in $line
+        a=$(echo $line | cut -d '=' -f2)
+        if [[ $a == *password* ]]; then
+            pwdlogin=1
+        fi
+        array[i]=$a
+        i=$((i+1))
+    done < $FILE
+    if [ $pwdlogin == 1 ]; then
+    
+    else
+        cmd="borg list ssh://${array[0]}@${array[1]}${array[2]}" 
+    fi
 }
 
-if  [[ $1 == "list" ]]
-then
-   sshpass -v -f $SSHPASSFILE -P $PATTERN borg list $BORGSERVER$REMOTEPATH
-elif [[ $1 == "data" ]]
-then
-   arg2err
-   sshpass -v -f $SSHPASSFILE -P $PATTERN borg list $BORGSERVER$REMOTEPATH::$2
-elif [[ $1 == "create" ]]
-then
-sshpass -v -f $SSHPASSFILE -P $PATTERN borg create -p --stats --compression zstd,15 --exclude='/dev' --exclude='/proc' --exclude='/sys' --exclude='/tmp' --exclude='/run' --exclude='/mnt' --exclude='/media' --exclude='/lost+found' --exclude='/bin' --exclude='/boot' --exclude='/lib' --exclude='/lib32' --exclude='/lib64' --exclude='/libx32' --exclude='/srv' --exclude='/sys' --exclude='/usr' $BORGSERVER$REMOTEPATH::$BACKUPNAME-{now:%Y-%m-%dT%H:%M:%S} '/'
-elif [[ $1 == "delete" ]]
-then
-   arg2err
-   sshpass -v -f $SSHPASSFILE -P $PATTERN borg delete $BORGSERVER$REMOTEPATH::$2 --stats -p
-elif [[ $1 == "extract" ]]
-then
-   arg2err
-   sshpass -v -f $SSHPASSFILE -P $PATTERN borg extract $BORGSERVER$REMOTEPATH::$2 --stats -p
-elif [[ $1 == "check" ]]
-then
-   retVal=$?
-   sshpass -v -f $SSHPASSFILE -P $PATTERN borg check $BORGSERVER$REMOTEPATH -p --verify-data
-   if [[ $retVal -eq 1 ]]
-   then
-       echo "Borg check completed. There are errors, exit code: "$retVal
-       exit 1
-   else
-       echo "Borg check completed. No errors, exit code: "$retVal
-   fi
-elif [[ $1 == "checkarch" ]]
-then
-   retVal=$?
-   arg2err
-   sshpass -v -f $SSHPASSFILE -P $PATTERN borg check $BORGSERVER$REMOTEPATH::$2 -p --verify-data
-   if [[ $retVal -eq 1 ]]
-   then
-       echo "Borg checkarch completed. There are errors, exit code: "$retVal
-       exit 1
-   else
-        echo "Borg checkarch completed. No errors, exit code: "$retVal
-   fi
-else
-   echo "No arguments supplied!"
+function setup(){
+
+    mkdir ~/.config/borghelper && touch ~/.config/borghelper/config.txt
+    echo "tell me the user and password of the backup server"
+    echo "user:"
+    read user
+    echo "ip or domain:"
+    read ip
+    echo "backup directory on the remote server"
+    read backupdir
+    echo "password or key login?"
+    read pok
+    if [ "$pok" = "password" ]; then
+        echo "password:"
+        read password
+    else
+        echo "keypath:"
+        read key
+    fi
+    echo "user=$user" >> ~/.config/borghelper/config.txt
+    echo "ip=$ip" >> ~/.config/borghelper/config.txt
+    echo "backupdir=$backupdir" >> ~/.config/borghelper/config.txt
+    if [ "$pok" = "password" ]; then
+        echo "password=$password" >> ~/.config/borghelper/config.txt
+    else
+        echo "key=$key" >> ~/.config/borghelper/config.txt
+    fi
+}
+
+FILE=~/.config/borghelper/config.txt
+if test -f "$FILE"; then
+    if [ "$1" = "reconfigure" ]; then
+        reconfigure
+    fi
+    if [ "$1" = "list" ]; then
+        list
+    fi
+else 
+    setup 
 fi
+
