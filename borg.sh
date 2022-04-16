5b68d9e6d4daff8214ddf29d255ea9d76db10050
rickytrevor <rick@rickyscloud.com>
2022-04-16 23:49:56 +0200

    added create backup

--- borg.sh
+++ borg.sh
@@ -12,6 +12,23 @@ function reconfigure(){
     rm $FILE
     setup 
 }
+function createbackup(){
+   #still to test
+   name=$2
+    declare -a array
+    i=0
+    pwdlogin=0
+    while IFS= read -r line; do
+        a=$(echo $line | cut -d '=' -f2)
+        if [[ $a == *password* ]]; then
+            export BORG_PASSCOMMAND="$a"
+        fi
+        array[i]=$a
+        i=$((i+1))
+    done < $FILE
+   cmd="borg create -p --stats --compression zstd,12 ssh://rick@backup.rickyscloud.com:22${array[3]}::deb-{now:%Y-%m-%d} '$name'"
+   echo $cmd
+}
 function list(){
     declare -a array
     i=0
@@ -19,17 +36,13 @@ function list(){
     while IFS= read -r line; do
         a=$(echo $line | cut -d '=' -f2)
         if [[ $a == *password* ]]; then
-            #could this work 🧐?
             export BORG_PASSCOMMAND="$a"
         fi
         array[i]=$a
         i=$((i+1))
     done < $FILE
-    if [ $pwdlogin == 1 ]; then
-    
-    else
-        cmd="borg list ssh://${array[0]}@${array[1]}${array[2]}" 
-    fi
+      cmd="borg list ssh://${array[0]}@${array[1]}:${array[2]}${array[3]}" 
+      $cmd 
 }
 
 function setup(){
@@ -40,6 +53,9 @@ function setup(){
     read user
     echo "ip or domain:"
     read ip
+    echo "backup directory on the remote server"
+    echo "port"
+    read port
     echo "backup directory on the remote server"
     read backupdir
     echo "password or key login?"
@@ -53,6 +69,7 @@ function setup(){
     fi
     echo "user=$user" >> ~/.config/borghelper/config.txt
     echo "ip=$ip" >> ~/.config/borghelper/config.txt
+    echo "port=$port" >> ~/.config/borghelper/config.txt
     echo "backupdir=$backupdir" >> ~/.config/borghelper/config.txt
     if [ "$pok" = "password" ]; then
         echo "password=$password" >> ~/.config/borghelper/config.txt
@@ -63,12 +80,15 @@ function setup(){
 
 FILE=~/.config/borghelper/config.txt
 if test -f "$FILE"; then
-    if [ "$1" = "reconfigure" ]; then
-        reconfigure
-    fi
-    if [ "$1" = "list" ]; then
-        list
-    fi
+   if [ "$1" = "reconfigure" ]; then
+      reconfigure
+   fi
+   if [ "$1" = "list" ]; then
+      list
+   fi
+   if [ "$1" = "backup" ]; then
+      createbackup $1 $2
+   fi
 else 
     setup 
 fi
